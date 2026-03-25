1f96001d53c209387981530bd791aaaea6b72f70
Shaktiman91 <makboy907@gmail.com>
2026-03-25 13:39:13 +0100

    fix: add accelerator_fft_pipelined.v to vlog compile list in run_behav_sim.sh

--- finaldesign_ee/sim_behav/run_behav_sim.sh
+++ finaldesign_ee/sim_behav/run_behav_sim.sh
@@ -10,11 +10,10 @@ rm -rf ${workLib}
 vlib ${workLib}
 vmap work ${workLib}
 
-# et4351_tb.v et4351.v accelerator.v spimemio.v simpleuart.v picosoc.v ../picorv32.v spiflash.v
 # Compile the DUT
 vlog /data/Cadence/gpdk045_v60/Synopsys_sram/saed32sram.v
-vlog ../src/design/accelerator.v \
-     ../src/design/accelerator_fft.v \
+vlog ../src/design/accelerator_fft_pipelined.v \
+     ../src/design/accelerator.v \
      ../src/design/accelerator_mem.v \
      ../src/design/picosoc.v \
      ../src/design/spimemio.v \
